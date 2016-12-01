1b887d4285a530552836690d530c38611fc0cd76
Dylan Shadbolt <jaemin1904@gmail.com>
2016-12-01 21:51:20 +1100

    Signing In

---
+++ js/loginValidation.js
@@ -0,0 +1,22 @@
+(function(){
+
+const txtUser = document.getElementById('txtUser');
+const txtPassword = document.getElementById('txtPassword');
+const btnSubmit = document.getElementById('submit');
+const btnLogout = document.getElementById('logout');
+console.log('lol')
+
+// Add login event
+btnSubmit.addEventListener('click', e => {
+    console.log('Authenticating User...')
+    //  Get username and password
+    const user = txtUser.value + "@visions.com";
+    console.log(user)
+    const pass = txtPassword.value;
+    const auth = firebase.auth();
+    // Sign In
+    const promise = auth.signInWithEmailAndPassword(email, pass);
+    prommise.catch(e => console.log(e.message));
+});
+
+});
